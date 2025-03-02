c820d6ed866670afbc7ac4cf570c3dffa25b27bd
MohamevallMoctar <medvalloper@hotmail.com>
2025-03-02 18:06:10 +0000

    Recipe Detail

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/recipe_detail.dart';
 import 'recipe.dart';
 void main() {
   runApp(const RecipesApp());
@@ -53,7 +54,20 @@ class _MyHomePageState extends State<MyHomePage> {
         ListView.builder(
           itemCount: Recipe.samples.length,
           itemBuilder: (BuildContext context, int index){
-            return buildRecipeCard(Recipe.samples[index]);
+            return GestureDetector(
+                onTap: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context){
+                            // TODO : REplacing with return RecipeDetail
+                            return RecipeDetail(recipe: Recipe.samples[index]);
+                          },
+                      )
+                  );
+                },
+                child: buildRecipeCard(Recipe.samples[index])
+            );
           },
         ),
       ),
